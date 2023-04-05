d4e1bc8905581126995257b4ed692a93ea6e43d3
sogolmansouri <smansou2@ncsu.edu>
2023-04-04 23:52:46 -0400

    exploratory plots

--- src/exploratory_analysis.py
+++ src/exploratory_analysis.py
@@ -7,8 +7,35 @@ import warnings
 import matplotlib.pyplot as plt
 
 sns.set(style='darkgrid')
+def do_analysis_usd_inrx():
+    data = pd.read_csv("../data/USDINRX.csv")
+    data['open-high'] = data['Open']-data['High']
+    data['open-low'] = data['Open'] - data['Low']
+    data['close-high'] = data['Close']-data['High']
+    data['close-low'] = data['Close'] - data['Low']
+    data['high-low'] = data['High'] - data['Low']
+    data['open-close'] = data['Open'] - data['Close']
+    data.head()
+    data2 = data.copy()
+    data2 = data2.drop(['Open','High','Low','Close', 'Adj Close'],axis=1)
+    
+    
+    print(data.info)
+    print(data.shape)
+    data = data.astype({'Date': 'datetime64[ns]'})
+    date = data['Date']
+    plt.figure(figsize=(16,8))
+    plt.title('Close Price History')
+    plt.plot(data['Date'],data['Close'])
+    plt.xlabel('Date')
+    plt.ylabel('Close Price INR (₹)')
+    plt.show()
 
-def do_analysis():
+    plt.figure(figsize=(7,5))
+    sns.heatmap(data2.corr(),cmap='Blues',annot=True)
+
+
+def do_analysis_forex():
     df = pd.read_csv("../data/forex.csv")
     print(df.shape)
     df['date'] = pd.to_datetime(df['date'])
@@ -18,6 +45,9 @@ def do_analysis():
     Cur1 = 'INR'
     df_IC = df[df['slug'].str.contains(Cur1 + '/')]
     scatter_plot(Cur1, df_IC)
+    Cur1 = 'USD'
+    df_IC = df[df['slug'].str.contains(Cur1 + '/')]
+    scatter_plot(df_IC)
 
 def scatter_plot(Cur1, df_IC):
     for Cur2 in df_IC['currency'].unique():
