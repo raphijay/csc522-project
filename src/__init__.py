5168cb657d53a28fea5512d4db67ad894d18c9d9
Rithik Jain <rjain25@ncsu.edu>
2023-04-13 10:53:00 -0400

    Added comments for _init_.py file

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+##
+# __init__.py file is an empty file that is used to mark a directory as a Python package. When Python
+# imports a package, it looks for the __init__.py file in the directory and executes it.
+# It can contain initialization code that should be executed when the package is imported. For example,
+# you can define variables, functions, classes, or import modules in the __init__.py file.
+##
