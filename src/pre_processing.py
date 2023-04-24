313626e308a98f42f38bd7ace33430f55c400f34
raphijay <raphilli67@gmail.com>
2023-04-24 17:55:31 -0400

    Fixed merge function in pre_processing

--- src/pre_processing.py
+++ src/pre_processing.py
@@ -70,10 +70,12 @@ def pre_p_usdinr(data):
 ##
 def merge(forex, usdinrx):
 
+    usdinrx = usdinrx.rename(columns={"Date": "date", "Open": "open", "High": "high", "Low": "low", "Close": "close"})
+
     merged_data = pd.concat([forex, usdinrx], axis = 0, ignore_index = True)
 
     # Drop any duplicate rows based on date
-    merged_data.drop_duplicates(subset = "date")
+    merged_data.drop_duplicates(subset = "date", inplace=True)
 
     return merged_data
 
